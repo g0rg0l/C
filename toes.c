6032e0bed99153b540aea52aeaf00d530317192d
Dmitry <70606894+g0rg0l@users.noreply.github.com>
2022-02-04 13:08:29 +0300

    Add files via upload

---
+++ toes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int toes = 10;
+
+    printf("toes = %i\n", toes);
+    printf("2 * toes = %i\n", 2 * toes);
+    printf("toes^2 = %i\n", toes * toes);
+
+    return 0;
+}
